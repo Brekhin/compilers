0dba4a8d2918f788109258a1dbc0fb0faf1fb5fc
Brekhin <brekhin@bk.ru>
2019-03-21 03:18:33 +0300

    regular expression to postfix form was implemented

--- main.go
+++ main.go
@@ -4,25 +4,61 @@ import (
 	"container/list"
 	"fmt"
 
-	"./utils"
+	"github.com/golang-collections/collections/stack"
 )
 
-func main() {
-	opstack := make(utils.Stack, 0)
-	var x list.List
-
-	x.PushBack(1)
-	x.PushBack(2)
-	for e := x.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value)
+func priority(ch string) int {
+	switch ch {
+	case "*":
+		return 4
+	case ".":
+		return 3
+	case "+":
+		return 2
+	case "|":
+		return 1
 	}
-	opstack = opstack.Push("1")
-	opstack = opstack.Push("2")
-	opstack = opstack.Push("3")
+	return 0
+}
 
-	opstack, p := opstack.Pop()
-	fmt.Println(opstack, p)
-	opstack, e := opstack.Pop()
+func convertRegexToPostfix(infix string) list.List {
 
-	fmt.Println(opstack, e)
+	opstack := stack.New()
+	var result list.List
+
+	for _, symbol := range infix {
+		switch {
+		case symbol == '(':
+			opstack.Push(string(symbol))
+		case symbol == ')':
+			for opstack.Peek() != "(" {
+				result.PushBack(opstack.Peek().(string))
+				opstack.Pop()
+			}
+			opstack.Pop()
+		case priority(string(symbol)) > 0:
+			for opstack.Len() > 0 && priority(string(symbol)) <= priority(opstack.Peek().(string)) {
+				result.PushBack(opstack.Peek().(string))
+				opstack.Pop()
+			}
+			opstack.Push(string(symbol))
+		default:
+			result.PushBack(string(symbol))
+		}
+	}
+
+	for opstack.Len() > 0 {
+		result.PushBack(opstack.Peek().(string))
+		opstack.Pop()
+	}
+
+	return result
+}
+
+func main() {
+	list := convertRegexToPostfix("(a.(b|d))*")
+	for e := list.Front(); e != nil; e = e.Next() {
+		fmt.Print(e.Value)
+	}
+	fmt.Println()
 }
